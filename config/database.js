2b0005187b14b8a1361accab843a50c958b9b65a
nazarkd <nazarkdh@gmail.com>
2024-04-30 15:12:35 +0430

    db password fixed

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 if(process.env.NODE_ENV=='production'){
   console.log('in production')
   module.exports={
-    mongoURI:'mongodb+srv://nazarkdh:<Deio9UqJDvJo3jHB>@vidjots-prod.1ctyjwx.mongodb.net/?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://nazarkdh:Deio9UqJDvJo3jHB@vidjots-prod.1ctyjwx.mongodb.net/?retryWrites=true&w=majority'
   }
 }else{
   module.exports={
-    mongoURI:'mongodb://localhost/vidjot-dev'
+    mongoURI:'mongodb+srv://nazarkdh:Deio9UqJDvJo3jHB@vidjots-prod.1ctyjwx.mongodb.net/?retryWrites=true&w=majority'
+    //mongoURI:'mongodb://localhost/vidjot-dev'
   }
 }
