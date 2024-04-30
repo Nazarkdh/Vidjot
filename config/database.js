33c09162a1bbd623c1972f21ff159421b4abed88
nazarkd <nazarkdh@gmail.com>
2024-04-30 15:30:01 +0430

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
-if(process.env.NODE_ENV=='production'){
-  console.log('in production')
+if(process.env.NODE_ENV==='production'){
+  console.log('1-in Node-env:',process.env.NODE_ENV)
   module.exports={
     mongoURI:'mongodb+srv://nazarkdh:Deio9UqJDvJo3jHB@vidjots-prod.1ctyjwx.mongodb.net/?retryWrites=true&w=majority'
   }
 }else{
+  console.log('2 in Node-env:',process.env.NODE_ENV)
   module.exports={
-   
-    mongoURI:'mongodb://localhost/vidjot-dev'
+    mongoURI:'mongodb+srv://nazarkdh:Deio9UqJDvJo3jHB@vidjots-prod.1ctyjwx.mongodb.net/?retryWrites=true&w=majority'
+    //mongoURI:'mongodb://localhost/vidjot-dev'
   }
 }
